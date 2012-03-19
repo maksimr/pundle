f234defce0c3135753d77759a90ce59592dd1f96
maxim <rv.maxim@gmail.com>
2012-03-19 12:02:16 +0400

    added update remove install

--- pundle-coffee.coffee
+++ pundle-coffee.coffee
@@ -2,47 +2,129 @@
 #  @name: pundle-coffee
 #  @description: Package manager for pentadactyl
 #  @version: 0.1
+#
+#  Windows:
+#  'rmdir /S /Q' :
 
 "use strict"
 
-bundleDir = io.getRuntimeDirectories('plugins/bundle').shift()
+bundleRoot = io.getRuntimeDirectories("plugins").shift().path
 
-bundles = bundleDir.readDirectory(true).filter (dir)->
-  dir.child(".git").exists()
+#BUNDLE
+class Bundle
+  constructor: (bundle) ->
+    if this not instanceof Bundle then return new Bundle(bundle)
 
+    #return package if package already exist
+    return pack for pack in Bundle.packages when pack.name == bundle
+
+    bundle = bundle.toString()
+    uri = "https://github.com/#{bundle}.git"
+    [author, name] = bundle.split('/')
+
+    dir = io.getRuntimeDirectories("plugins").shift().child(name)
+
+    @uri = uri
+    @path = dir.path
+    @author = author
+    @name = name
+    @instaled = dir.exists()
+
+    Bundle.packages.push this
+
+Bundle.packages = []
+
+BundleInstall = (args)->
+  packName = args.toString()
+  pundle.install(packName)
+
+BundleUpdate = (args)->
+  packName = args.toString()
+  pundle.update(packName)
+
+BundleRemove = (args)->
+  packName = args.toString()
+  pundle.remove(packName)
+
+#PUNDLE
 pundle =
   info:
     version: "0.1"
     description: "Package manager for pentadactyl"
 
-  execute: (args) ->
-    dirs = args['-directories'] or args['-D']
-    dirs = if dirs then @directories.filter (dir)-> (dirs.indexOf(dir.leafName) >=0) else @directories
-    cmd = "#{args}" or "status"
-    fn = @_call.bind this, cmd
+  plugins: dactyl.pluginFiles
+  cmd:
+    INSTALL: "git clone"
+    UPDATE: "git pull"
+    REMOVE: 'rm -rf'
 
-    dirs.forEach fn
-  _call: (command, dir)->
+  install: (packName)->
+    packages = if packName then [Bundle(packName)] else Bundle.packages
+
     _cwd = io.cwd
-    try
-      io.cwd = dir.path
-      output = io.system "git #{command}"
-    catch error
-      output = error
-    dactyl.echomsg "OCTOPUS: #{dir.leafName.toUpperCase()} >> #{output}"
+    io.cwd = bundleRoot
+
+    cmd = @cmd.INSTALL
+    log = @log
+
+    packages.forEach (pack)->
+      unless pack.instaled
+        output = pundle.exec(pack, "#{cmd} #{pack.uri}")
+        log(pack.name, output)
     io.cwd = _cwd
 
 
+  update: (packName)->
+    packages = if packName then (pack for pack in Bundle.packages when pack.name == packName) else Bundle.packages
+
+    _cwd = io.cwd
+    cmd = @cmd.UPDATE
+    log = @log
+
+    packages.forEach (pack)->
+      if pack.instaled
+        io.cwd = pack.path
+        output = pundle.exec(pack, "#{cmd}")
+        log(pack.name, output)
+    io.cwd = _cwd
+
+
+  remove: (packName)->
+    packages = Bundle.packages
+    pack = p for p in packages when p.name == packName
+
+    index = packages.indexOf(pack)
+    packages.splice(index,index)
+    cmd = "#{@cmd.REMOVE} #{pack.path}"
+
+    output = pundle.exec(pack, cmd)
+
+    @log(pack.name, output)
+
+  exec: (pack, cmd)->
+    try
+      output = io.system cmd
+    catch error
+      output = error
+    output
+
+  log: (spec, output) ->
+    dactyl.echomsg " "
+    dactyl.echomsg "$ #{spec}"
+    dactyl.echomsg "> #{output}"
+
+  completer: (context)->
+    context.completions = ([pack.name, pack.name] for pack in Bundle.packages)
+
 
 #DECLARE PLUGIN
-group.commands.add ["pundle", "pud"], pundle.info.description, (args)->
-  pundle.execute(args)
-,{
+group.commands.add ["Bundle", "bund"], pundle.info.description, Bundle
+
+group.commands.add ["BundleInstall", "bundi"], pundle.info.description, BundleInstall
+group.commands.add ["BundleUpdate", "bundu"], pundle.info.description, BundleUpdate,
   literal: 0
-  options: [{
-    names: ["-directories","-D"]
-    description: "Select specific directories"
-    type: CommandOption.LIST
-    completer: bundles.map (dir)-> [dir.leafName, dir.path]
-  }]
-}
+  completer: pundle.completer
+
+group.commands.add ["BundleRemove", "bundr"], pundle.info.description, BundleRemove,
+  literal: 0
+  completer: pundle.completer
---
+++ pundle-coffee.js
@@ -0,0 +1,161 @@
+(function() {
+  "use strict";
+  var Bundle, BundleInstall, BundleRemove, BundleUpdate, bundleRoot, pundle;
+
+  bundleRoot = io.getRuntimeDirectories("plugins").shift().path;
+
+  Bundle = (function() {
+
+    function Bundle(bundle) {
+      var author, dir, name, pack, uri, _i, _len, _ref, _ref2;
+      if (!(this instanceof Bundle)) return new Bundle(bundle);
+      _ref = Bundle.packages;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        pack = _ref[_i];
+        if (pack.name === bundle) return pack;
+      }
+      bundle = bundle.toString();
+      uri = "https://github.com/" + bundle + ".git";
+      _ref2 = bundle.split('/'), author = _ref2[0], name = _ref2[1];
+      dir = io.getRuntimeDirectories("plugins").shift().child(name);
+      this.uri = uri;
+      this.path = dir.path;
+      this.author = author;
+      this.name = name;
+      this.instaled = dir.exists();
+      Bundle.packages.push(this);
+    }
+
+    return Bundle;
+
+  })();
+
+  Bundle.packages = [];
+
+  BundleInstall = function(args) {
+    var packName;
+    packName = args.toString();
+    return pundle.install(packName);
+  };
+
+  BundleUpdate = function(args) {
+    var packName;
+    packName = args.toString();
+    return pundle.update(packName);
+  };
+
+  BundleRemove = function(args) {
+    var packName;
+    packName = args.toString();
+    return pundle.remove(packName);
+  };
+
+  pundle = {
+    info: {
+      version: "0.1",
+      description: "Package manager for pentadactyl"
+    },
+    plugins: dactyl.pluginFiles,
+    cmd: {
+      INSTALL: "git clone",
+      UPDATE: "git pull",
+      REMOVE: 'rm -rf'
+    },
+    install: function(packName) {
+      var cmd, log, packages, _cwd;
+      packages = packName ? [Bundle(packName)] : Bundle.packages;
+      _cwd = io.cwd;
+      io.cwd = bundleRoot;
+      cmd = this.cmd.INSTALL;
+      log = this.log;
+      packages.forEach(function(pack) {
+        var output;
+        if (!pack.instaled) {
+          output = pundle.exec(pack, "" + cmd + " " + pack.uri);
+          return log(pack.name, output);
+        }
+      });
+      return io.cwd = _cwd;
+    },
+    update: function(packName) {
+      var cmd, log, pack, packages, _cwd;
+      packages = packName ? (function() {
+        var _i, _len, _ref, _results;
+        _ref = Bundle.packages;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          pack = _ref[_i];
+          if (pack.name === packName) _results.push(pack);
+        }
+        return _results;
+      })() : Bundle.packages;
+      _cwd = io.cwd;
+      cmd = this.cmd.UPDATE;
+      log = this.log;
+      packages.forEach(function(pack) {
+        var output;
+        if (pack.instaled) {
+          io.cwd = pack.path;
+          output = pundle.exec(pack, "" + cmd);
+          return log(pack.name, output);
+        }
+      });
+      return io.cwd = _cwd;
+    },
+    remove: function(packName) {
+      var cmd, index, output, p, pack, packages, _i, _len;
+      packages = Bundle.packages;
+      for (_i = 0, _len = packages.length; _i < _len; _i++) {
+        p = packages[_i];
+        if (p.name === packName) pack = p;
+      }
+      index = packages.indexOf(pack);
+      packages.splice(index, index);
+      cmd = "" + this.cmd.REMOVE + " " + pack.path;
+      output = pundle.exec(pack, cmd);
+      return this.log(pack.name, output);
+    },
+    exec: function(pack, cmd) {
+      var output;
+      try {
+        output = io.system(cmd);
+      } catch (error) {
+        output = error;
+      }
+      return output;
+    },
+    log: function(spec, output) {
+      dactyl.echomsg(" ");
+      dactyl.echomsg("$ " + spec);
+      return dactyl.echomsg("> " + output);
+    },
+    completer: function(context) {
+      var pack;
+      return context.completions = (function() {
+        var _i, _len, _ref, _results;
+        _ref = Bundle.packages;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          pack = _ref[_i];
+          _results.push([pack.name, pack.name]);
+        }
+        return _results;
+      })();
+    }
+  };
+
+  group.commands.add(["Bundle", "bund"], pundle.info.description, Bundle);
+
+  group.commands.add(["BundleInstall", "bundi"], pundle.info.description, BundleInstall);
+
+  group.commands.add(["BundleUpdate", "bundu"], pundle.info.description, BundleUpdate, {
+    literal: 0,
+    completer: pundle.completer
+  });
+
+  group.commands.add(["BundleRemove", "bundr"], pundle.info.description, BundleRemove, {
+    literal: 0,
+    completer: pundle.completer
+  });
+
+}).call(this);
